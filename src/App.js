50f1f4416fb1f3f27229f86f4e53bebed18790c7
TechnoM4ncer <kinodekirakashi@live.com>
2022-01-23 21:21:48 -0600

    Fix real name

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { Component } from 'react';
+import logo from './logo.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let counter = 1;
+
+const api = axios.create({
+	baseURL: 'https://stormy-journey-82781.herokuapp.com/https://superheroapi.com/api/1525724057763659/70'
+})
+
+class App extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			heroes: []
+			}
+		}	
+
+		componentDidMount() {
+			api.get('/').then(res => {
+				let heroes = [res.data];
+				this.setState({ heroes: heroes })
+			})
+	}
+		render() {
+			return (
+				<div className="App">
+					<header>
+
+					<img id="leftImg" width = "5%" height = "5%" src={logo} alt="Logo"/>
+					<img id="rightImg" width = "5%" height = "5%" src={logo} alt="Logo"/>
+
+					<center>
+
+					<h1>Welcome to the site!</h1>
+					<h2>Here is a list of super heroes from DC Comics!</h2>
+
+					<hr />
+					</center>
+					</header>
+			<body>
+
+			<div><center>
+
+			{this.state.heroes.map(hero => <div key={hero.id}>
+				<h2>{hero.name}</h2> 
+				<img src={hero.image.url}></img>
+			<p>Real Name: {hero.biography.["full-name"]}</p>
+			<h2>Stats: </h2>
+			<p>
+				Combat: {hero.powerstats.combat} <br />
+				Durability: {hero.powerstats.durability} <br />
+				Intelligence: {hero.powerstats.intelligence} <br />
+				Power: {hero.powerstats.power} <br />
+				Speed: {hero.powerstats.speed} <br />
+				Strength: {hero.powerstats.strength}
+			</p>
+			</div>
+			)}
+			
+						
+			<hr />
+
+			</center>
+			</div>	
+			</body>
+			<hr />
+
+				<footer>
+				<center>
+
+					Designed by TechnoM4ncer in 2021
+
+				<hr />
+
+				</center>
+			</footer>
+		</div>
+	)}
 }
 
 export default App;
